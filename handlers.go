96318c0db2202c11cc19763a4d52357b5279ae1b
Matt Silverlock <matt@eatsleeprepeat.net>
2015-07-04 23:12:50 +0800

    Moved the package godoc into a separate doc.go file. Expanded on it.

--- handlers.go
+++ handlers.go
@@ -1,10 +1,6 @@
 // Copyright 2013 The Gorilla Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
-/*
-Package handlers is a collection of handlers for use with Go's net/http package.
-*/
 package handlers
 
 import (
