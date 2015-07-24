60d6d176c9473fd841494612e91abdd049f740c3
Eli Goldman <egoldman@lclark.edu>
2015-07-24 11:09:51 -0700

    Including query string in log

--- handlers.go
+++ handlers.go
@@ -26,7 +26,7 @@ import (
 
 func LogWrapper(handlerToWrap http.HandlerFunc, logger *log.Log) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		formatStr := "\n%v %v %v\nHost: %v\nUser-Agent: %v\nContent-Length: %v\n%v\n%v"
+		formatStr := "\n%v %v%v %v\nHost: %v\nUser-Agent: %v\nContent-Length: %v\n%v\n%v"
 		var headerStr string
 		for headerName, headerValueStringSlice := range r.Header {
 			for _, headerValue := range headerValueStringSlice {
@@ -37,7 +37,11 @@ func LogWrapper(handlerToWrap http.HandlerFunc, logger *log.Log) http.HandlerFun
 		if err != nil {
 			logger.Error("could not read request body")
 		} else {
-			logger.Debug(fmt.Sprintf(formatStr, r.Method, r.URL.Path, r.Proto, r.Host, r.UserAgent(), r.ContentLength, headerStr, string(bodyBytes)))
+			var queryString string
+			if r.URL.RawQuery != "" {
+				queryString = "?" + r.URL.RawQuery
+			}
+			logger.Debug(fmt.Sprintf(formatStr, r.Method, r.URL.Path, queryString, r.Proto, r.Host, r.UserAgent(), r.ContentLength, headerStr, string(bodyBytes)))
 		}
 
 		r.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
