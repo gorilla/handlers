f7731994851a8b00d742b6711dbe840ea982833e
fangdingjun <fangdingjun@gmail.com>
2015-11-09 16:46:08 +0800

    log right requestURI in proxy mode

--- handlers.go
+++ handlers.go
@@ -250,7 +250,10 @@ func buildCommonLogLine(req *http.Request, url url.URL, ts time.Time, status int
 		host = req.RemoteAddr
 	}
 
-	uri := url.RequestURI()
+	uri := req.RequestURI
+	if req.ProtoMajor == 2 && req.Method == "CONNECT" {
+		uri = req.Host
+	}
 
 	buf := make([]byte, 0, 3*(len(host)+len(username)+len(req.Method)+len(uri)+len(req.Proto)+50)/2)
 	buf = append(buf, host...)
